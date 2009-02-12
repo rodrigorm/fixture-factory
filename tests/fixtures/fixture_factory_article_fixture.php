cd60c07abf9dfecba114470d4363b22cb1bf4e13
Michał Szajbe <michal.szajbe@gmail.com>
2009-02-12 19:26:13 +0100

    Initial commit

---
+++ tests/fixtures/fixture_factory_article_fixture.php
@@ -0,0 +1,19 @@
+<?php
+App::import('Model', array('AppModel'));
+class Article extends AppModel {
+  var $name = 'Article';
+  var $belongsTo = array(
+    'User'
+  );
+}
+
+class FixtureFactoryArticleFixture extends CakeTestFixture {
+  var $name = 'Article';
+  var $table = 'articles';
+  var $fields = array(
+    'id'      => array('type' => 'integer', 'key' => 'primary'),
+    'title'   => array('type' => 'string', 'null' => false),
+    'user_id' => array('type' => 'integer', 'null' => true)
+  );
+}
+?>
---
+++ tests/fixtures/fixture_factory_user_fixture.php
@@ -0,0 +1,20 @@
+<?php
+App::import('Model', array('AppModel'));
+class User extends AppModel {
+  var $name = 'User';
+  var $hasMany = array(
+    'Article'
+  );
+}
+
+class FixturefactoryUserFixture extends CakeTestFixture {
+  var $name = 'User';
+  var $table = 'users';
+  var $fields = array(
+    'id'      => array('type' => 'integer', 'key' => 'primary'),
+    'login'   => array('type' => 'string', 'null' => false),
+    'active'  => array('type' => 'boolean', 'null' => true),
+    'gender'  => array('type' => 'string', 'length' => 1, 'null' => true)
+  );
+}
+?>
